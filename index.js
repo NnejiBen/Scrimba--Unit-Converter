398baccc3c4a2dc17bf50ab748ce343399cd0561
Benjamin Nneji <nnejiben@gmail.com>
2023-03-15 17:30:39 +0100

    span for input

--- index.js
+++ index.js
@@ -3,22 +3,30 @@ const convertBtn = document.getElementById("convert-btn")
 const lengthEl = document.getElementById("length-el")
 const volumeEl = document.getElementById("volume-el")
 const massEl = document.getElementById("mass-el")
+const clearBtn = document.getElementById("clear-btn")
 
 convertBtn.addEventListener("click", function() {
-    lengthEl.textContent = `${inputEl.value} meters = 
-    ${(inputEl.value * 3.281).toFixed(3)} feet | 
-    ${inputEl.value} feet = 
-    ${(inputEl.value / 3.281).toFixed(3)} meters`
+    lengthEl.textContent = `${inputEl.textContent} meters = 
+    ${(inputEl.textContent * 3.281).toFixed(3)} feet | 
+    ${inputEl.textContent} feet = 
+    ${(inputEl.textContent / 3.281).toFixed(3)} meters`
 
-    volumeEl.textContent = `${inputEl.value} liters = 
-    ${(inputEl.value * 0.264).toFixed(3)} gallons | 
-    ${inputEl.value} gallons = 
-    ${(inputEl.value / 0.264).toFixed(3)} liters`
+    volumeEl.textContent = `${inputEl.textContent} liters = 
+    ${(inputEl.textContent * 0.264).toFixed(3)} gallons | 
+    ${inputEl.textContent} gallons = 
+    ${(inputEl.textContent / 0.264).toFixed(3)} liters`
 
-    massEl.textContent = `${inputEl.value} kilos = 
-    ${(inputEl.value * 2.204).toFixed(3)} pounds | 
-    ${inputEl.value} pounds = 
-    ${(inputEl.value / 2.204).toFixed(3)} kilos`
+    massEl.textContent = `${inputEl.textContent} kilos = 
+    ${(inputEl.textContent * 2.204).toFixed(3)} pounds | 
+    ${inputEl.textContent} pounds = 
+    ${(inputEl.textContent / 2.204).toFixed(3)} kilos`
+})
+
+clearBtn.addEventListener("click", function() {
+    inputEl.textContent = ""
+    lengthEl.textContent = ""
+    volumeEl.textContent = ""
+    massEl.textContent = ""
 })
 
 /*
